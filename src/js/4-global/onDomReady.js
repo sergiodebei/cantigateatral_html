fad6abcec0fd75ab81d8fef71d7a99fcdb82160c
sergio_de_bei <sergiodebei84@gmai.com>
2018-09-09 21:27:35 +0200

    Add click on menu and scroll function

--- src/js/4-global/onDomReady.js
+++ src/js/4-global/onDomReady.js
@@ -1,3 +1,19 @@
 $(document).on("ready", function () {
-	// Website wide events
+	//hamburger menu
+	$('.hamburger-menu').on('click', function() {
+        $(this).toggleClass('open');
+        $('nav').toggleClass('open');
+	});
+
+	var header = $("header");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 300) {
+            header.addClass("darkHeader");
+        } else {
+            header.removeClass("darkHeader");
+        }
+    });
 });
+
